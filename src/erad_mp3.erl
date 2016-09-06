eda5bdbc82a37fca4e5e69e54e80f58e71736f26
Ilya Ashchepkov <koctep@gmail.com>
2016-09-07 02:00:00 +0700

    erad_mp3: remove unused vars and functions, add frame duration, return frame info

--- src/erad_mp3.erl
+++ src/erad_mp3.erl
@@ -3,17 +3,15 @@
 -export([open/1]).
 -export([read_frame/1]).
 
--compile([export_all]).
-
 -type fd() :: any().
 
 -spec open(binary()) -> {'ok', fd()} | {'error', any()}.
 open(Filename) ->
-  file:open(Filename, ['read', 'raw', 'binary']).
+  erad_file:open(Filename).
 
 -spec read_frame(fd()) -> {'ok', binary()} | 'eof' | {'error', any()}.
 read_frame(File) ->
-  case file:read(File, 4) of
+  case read(File, 4) of
     {'ok', Data} ->
       read_frame(File, Data);
     Else ->
@@ -25,39 +23,46 @@ read_frame(File, <<16#ff:8,             % 24-31
                    2#111:3,             % 21-23
                    MpegIdMask:2,        % 19-20
                    LayerMask:2,         % 17-18
-                   Crc:1,               % 16
+                   _Crc:1,               % 16
                    BitRateMask:4,       % 12-15
                    SampleRateMask:2,    % 10-11
                    Padding:1,           % 9
-                   Priv:1,              % 8
-                   Mode:2,              % 6-7
-                   ModeExt:2,           % 4-5
-                   Copyrighted:1,       % 3
-                   Original:1,          % 2
-                   Emphasis:2           % 0-1
-                 >>) ->
+                   _Priv:1,              % 8
+                   _Mode:2,              % 6-7
+                   _ModeExt:2,           % 4-5
+                   _Copyrighted:1,       % 3
+                   _Original:1,          % 2
+                   _Emphasis:2           % 0-1
+                 >> = Header) ->
   lager:debug("reading frame"),
   MpegId = mpeg_id(MpegIdMask),
   Layer = layer(LayerMask),
   BitRate = bit_rate(MpegId, Layer, BitRateMask),
   SampleRate = sample_rate(MpegId, SampleRateMask),
   FrameSize = frame_size(MpegId, Layer),
-  PaddingSize = padding_size(Layer),
-  FrameLen = (BitRate * 125 * FrameSize) div SampleRate + Padding * PaddingSize - 4,
+  PaddingSize = slot_size(Layer),
+  FrameLen = (BitRate * 125 * FrameSize) div SampleRate + Padding * PaddingSize,
+  FrameDuration = (FrameSize * 1000) div SampleRate,
   lager:debug("frame size  ~p", [FrameSize]),
   lager:debug("bit rate    ~p", [BitRate]),
   lager:debug("sample rate ~p", [SampleRate]),
   lager:debug("frame len   ~p", [FrameLen]),
   lager:debug("padding     ~p", [Padding]),
-  case file:read(File, FrameLen) of
+  FrameInfo = #{mpeg_id => MpegId,
+                layer   => Layer,
+                bitrate => BitRate,
+                sample_rate => SampleRate,
+                frame_duration => FrameDuration
+               },
+  case read(File, FrameLen - 4) of
     {'ok', Data} ->
       lager:debug("read ~p", ['_':to_hex(Data)]),
-      {'frame', Data};
+      {'frame', FrameInfo, <<Header/binary, Data/binary>>};
     Else -> Else
   end;
 read_frame(File, <<"TAG+">>) ->
   lager:debug("reading id3v1"),
-  case file:read(File, 227 - 4) of
+  case read(File, 227 - 4) of
     {'ok', Data} ->
       {'id3', Data};
     Else ->
@@ -65,7 +70,7 @@ read_frame(File, <<"TAG+">>) ->
   end;
 read_frame(File, <<"TAG", Byte:8>>) ->
   lager:debug("reading id3"),
-  case file:read(File, 128 - 4) of
+  case read(File, 128 - 4) of
     {'ok', Data} ->
       {'id3', <<Byte:8, Data/binary>>};
     Else ->
@@ -73,11 +78,11 @@ read_frame(File, <<"TAG", Byte:8>>) ->
   end;
 read_frame(File, <<"ID3", VerH:8>>) ->
   lager:debug("reading id3v2"),
-  case file:read(File, 10 - 4) of
+  case read(File, 10 - 4) of
     {'ok', <<VerL:8, Flags:8, 0:1, S1:7, 0:1, S2:7, 0:1, S3:7, 0:1, S4:7>>} ->
       <<_E:4, Size:24>> = <<S1:7, S2:7, S3:7, S4:7>>,
       lager:debug("id3v2 size is ~p (~p)", [Size, _E]),
-      case file:read(File, Size) of
+      case read(File, Size) of
         {'ok', Data} ->
           {'id3', <<VerH:8, VerL:8, Flags:1, Size:4, Data/binary>>};
         Else ->
@@ -86,7 +91,7 @@ read_frame(File, <<"ID3", VerH:8>>) ->
     Else ->
       Else
   end;
-read_frame(File, Data) ->
+read_frame(_File, Data) ->
   lager:alert("unmatched ~p", ['_':to_hex(Data)]).
 
 mpeg_id(2#00) -> throw({{'badarg', 'mpeg_id'}, 2.5});
@@ -105,7 +110,8 @@ frame_size(2, 2) -> 1152;
 frame_size(2, 3) -> 576.
 
 slot_size(1) -> 4;
-slot_size(_) -> 1.
+slot_size(2) -> 1;
+slot_size(3) -> 1.
 
 bit_rate(Id, Layer, 0) -> throw({{'badarg', 'mask'}, #{mpeg => Id, layer => Layer, bit_rate_mask => 0}});
 bit_rate(2, 3, BitRateMask) -> bit_rate(2, 2, BitRateMask);
@@ -121,8 +127,8 @@ bit_rate(1, 3, Mask) when Mask < 2#1110  -> (Mask - 2#1001) * 32 + 128;
 bit_rate(1, 3, 2#1110)                   -> 320;
 
 bit_rate(_Id, _Layer, 2#0001)            -> 32;
-bit_rate(Id, Layer,   2#0010)            -> 48;
-bit_rate(Id, Layer,   2#0011)                                   -> 56;
+bit_rate(_Id, _Layer, 2#0010)            -> 48;
+bit_rate(_Id, _Layer, 2#0011)            -> 56;
 
 bit_rate(_, _, Mask) when Mask =< 2#0111 -> (Mask - 2#0100) * 16 + 64;
 
@@ -141,13 +147,7 @@ sample_rate(2, 2#00) -> 22050;
 sample_rate(2, 2#01) -> 24000;
 sample_rate(2, 2#11) -> 16000.
 
-padding_size(1) -> 4;
-padding_size(2) -> 1;
-padding_size(3) -> 1.
-
-frame_len(1, Padding, BitRate, SampleRate) ->
-  lager:debug("padding ~p", [Padding]),
-  (48 * BitRate) rem SampleRate + Padding * 4;
-frame_len(Layer, Padding, BitRate, SampleRate) when Layer =:= 2 orelse Layer =:= 3 ->
-  lager:debug("padding ~p", [Padding]),
-  (144 * BitRate) rem SampleRate + Padding.
+read({Module, File}, Len) ->
+  Module:read(File, Len);
+read(File, Len) ->
+  read({file, File}, Len).
